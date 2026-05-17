82c8ece6944b80e419e1f777ec33f3aaae6a9e88
chubati <cristihubati03@gmail.com>
2026-05-17 16:55:28 +0300

    feature: add project files with basic dependecies

---
+++ src/main/java/com/maciu19/jmix2springboot/Jmix2springbootApplication.java
@@ -0,0 +1,13 @@
+package com.maciu19.jmix2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jmix2springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jmix2springbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciu19/jmix2springboot/Jmix2springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciu19.jmix2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jmix2springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
